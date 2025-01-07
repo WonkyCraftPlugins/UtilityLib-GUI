19ed46a8ad7b2f27b82931844cf90119a4f6d587
Wonkglorg <Wonkglorg.18@gmail.com>
2025-01-07 22:08:12 +0100

    Added all filler colors

--- src/main/java/com/wonkglorg/utilitylib/inventory/GuiInventory.java
+++ src/main/java/com/wonkglorg/utilitylib/inventory/GuiInventory.java
@@ -54,18 +54,23 @@ public abstract class GuiInventory<T extends MenuProfile> implements Listener{
 		}
 	}
 	
-	/**
-	 * A gray stained glass pane with no name. Good for filling empty slots in GUIs.
-	 */
-	public static final ItemStack FILLER;
-	
-	static {
-		FILLER = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
-		ItemMeta meta = FILLER.getItemMeta();
-		meta.setHideTooltip(true);
-		meta.displayName(Component.empty());
-		FILLER.setItemMeta(meta);
-	}
+	public static final ItemStack BLACK_FILLER = createFiller(Material.BLACK_STAINED_GLASS_PANE);
+	public static final ItemStack GRAY_FILLER = createFiller(Material.GRAY_STAINED_GLASS_PANE);
+	public static final ItemStack WHITE_FILLER = createFiller(Material.WHITE_STAINED_GLASS_PANE);
+	public static final ItemStack RED_FILLER = createFiller(Material.RED_STAINED_GLASS_PANE);
+	public static final ItemStack GREEN_FILLER = createFiller(Material.GREEN_STAINED_GLASS_PANE);
+	public static final ItemStack BLUE_FILLER = createFiller(Material.BLUE_STAINED_GLASS_PANE);
+	public static final ItemStack YELLOW_FILLER = createFiller(Material.YELLOW_STAINED_GLASS_PANE);
+	public static final ItemStack ORANGE_FILLER = createFiller(Material.ORANGE_STAINED_GLASS_PANE);
+	public static final ItemStack PURPLE_FILLER = createFiller(Material.PURPLE_STAINED_GLASS_PANE);
+	public static final ItemStack CYAN_FILLER = createFiller(Material.CYAN_STAINED_GLASS_PANE);
+	public static final ItemStack PINK_FILLER = createFiller(Material.PINK_STAINED_GLASS_PANE);
+	public static final ItemStack LIME_FILLER = createFiller(Material.LIME_STAINED_GLASS_PANE);
+	public static final ItemStack LIGHT_BLUE_FILLER = createFiller(Material.LIGHT_BLUE_STAINED_GLASS_PANE);
+	public static final ItemStack MAGENTA_FILLER = createFiller(Material.MAGENTA_STAINED_GLASS_PANE);
+	public static final ItemStack BROWN_FILLER = createFiller(Material.BROWN_STAINED_GLASS_PANE);
+	public static final ItemStack LIGHT_GRAY_FILLER = createFiller(Material.LIGHT_GRAY_STAINED_GLASS_PANE);
+	public static final ItemStack CYAN_STAINED_GLASS_PANE = createFiller(Material.CYAN_STAINED_GLASS_PANE);
 	
 	protected final JavaPlugin plugin;
 	private final Inventory inventory;
@@ -806,5 +811,19 @@ public abstract class GuiInventory<T extends MenuProfile> implements Listener{
 	
 	public void updateClickHandlers() {
 		clickHandlers.sort(Comparator.comparingInt(ClickActionData::weight));
+	}
+	
+	/**
+	 * Utility Method to create a filler item for a gui (removes tooltip)
+	 *
+	 * @param material The material to create the filler ItemStack with
+	 * @return The filler ItemStack
+	 */
+	public static ItemStack createFiller(Material material) {
+		ItemStack filler = new ItemStack(material);
+		ItemMeta meta = filler.getItemMeta();
+		meta.setHideTooltip(true);
+		filler.setItemMeta(meta);
+		return filler;
 	}
 }
